5996d8b0ab8a3e79a48302b646de5b4efc81e407
angelavanderklugt <56338270+angelavanderklugt@users.noreply.github.com>
2019-10-24 13:13:12 +1030

    Update Hunter.cpp

--- Hunter.cpp
+++ Hunter.cpp
@@ -2,21 +2,21 @@
 #include <string>
 #include "Hunter.h"
 using namespace std;
-
+//Default hunter class
 Hunter::Hunter() {
 	Name = "?";
 	Count = 0;
 	ItemNumber = 0;
 	Occup = "Classified";
 }
-
+//Hunter that takes name and job.
 Hunter::Hunter(string PlayName, string Job) {
 	Name = PlayName;
 	Count = 0;
 	ItemNumber = 0;
 	Occup = Job;
 }
-
+//Will keep item in placeholder when an item is obtained. Uses dynamic arrays.
 void Hunter::AddItem(string Item) {
 	
 	string * PlaceHolder = new string[Count+1];
@@ -33,19 +33,19 @@ void Hunter::AddItem(string Item) {
 	Items[Count-1]= Item;
 	ItemNumber++;
 }
-
+//Uses pointer, returning the item/items.
 string * Hunter::getItems() {
 	return Items;
 }
-
+//Returns the number of items.
 int Hunter::getItemCount() {
 	return ItemNumber;
 }
-
+//Returns player name.
 string Hunter::getName() {
 	return Name;
 }
-
+//Returns you job.
 string Hunter::getOccup() {
 	return Occup;
 }
